36c513f865d06e7241521b12e5813ad291a789a8
ChrisUustal <christopheruustal@gmail.com>
2022-10-20 23:35:30 -0500

    more testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
  *
  */
 
+uint32_t prev_tick = 0U;
+
 #if defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
 #include "teensy_can.h"
 // The bus number is a template argument for Teensy: TeensyCAN<bus_num>
@@ -26,10 +28,11 @@ ESPCAN can_bus{};
  * There are no constructor arguments
  *
  */
-CANSignal<uint16_t, 0, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> engine_rpm_signal{};
-CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> inlet_manifold_pressure_signal{};
-CANSignal<float, 32, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), true> inlet_manifold_temperature_signal{};
-CANSignal<float, 48, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> throttle_position_signal{};
+CANSignal<uint16_t, 0, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false, ICANSignal::ByteOrder::kBigEndian> engine_rpm_signal{};
+CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false, ICANSignal::ByteOrder::kBigEndian> inlet_manifold_pressure_signal{};
+CANSignal<float, 32, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), true, ICANSignal::ByteOrder::kBigEndian> inlet_manifold_temperature_signal{};
+CANSignal<float, 48, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false, ICANSignal::ByteOrder::kBigEndian> throttle_position_signal{};
+CANSignal<float, 40, 8, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false, ICANSignal::ByteOrder::kBigEndian> ecu_bat_voltage_signal{};
 // CANSignal<uint8_t, 16, 8, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> uint8_t_tx_signal{};
 // CANSignal<bool, 24, 1, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> bool_tx_signal{};
 // CANSignal<uint32_t, 32, 32, CANTemplateConvertFloat(1), CANTemplateConvertFloat(0), false> millis_tx_signal{};
@@ -49,17 +52,22 @@ CANSignal<float, 48, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0
  * CANTXMessage takes the CAN bus to transmit on, the message ID, the message size in bytes (based on the end position of the highest signal), the transmit period, and the signals as arguments
  *
  */
-// CANTXMessage<4> tx_message{can_bus, 0x100, 8, std::chrono::milliseconds{100}, float_tx_signal, uint8_t_tx_signal, bool_tx_signal, millis_tx_signal};
+// CANTXMessage<4> tx_message{can_bus, 0x640, 8, std::chrono::milliseconds{100}, engine_rpm_signal, 
+//                                            inlet_manifold_pressure_signal, 
+//                                            inlet_manifold_temperature_signal, 
+//                                            throttle_position_signal};
 
 /**
  * CANRXMessage takes the CAN bus to receive on, the message ID, and the signals to be received as constructor arguments
  * CANRXMessages automatically register themselves with the can_bus on construction
  *
  */
-CANRXMessage<4> rx_message{can_bus, 0x640, engine_rpm_signal, 
+CANRXMessage<4> rx_message_640{can_bus, 0x640, engine_rpm_signal, 
                                            inlet_manifold_pressure_signal, 
                                            inlet_manifold_temperature_signal, 
                                            throttle_position_signal};
+
+CANRXMessage<1> rx_message_649{can_bus, 0x649, ecu_bat_voltage_signal};
 
 void setup()
 {
@@ -67,11 +75,13 @@ void setup()
    * The CAN bus(es) need to be initialized with their baud rate
    *
    */
-  can_bus.RegisterRXMessage(rx_message);
+  can_bus.RegisterRXMessage(rx_message_649);
   can_bus.Initialize(ICAN::BaudRate::kBaud1M);
 
   Serial.begin(9600);
   Serial.println("Started");
+
+  prev_tick = millis();
 }
 
 std::chrono::milliseconds next_tick_time = std::chrono::milliseconds(millis());
@@ -79,29 +89,28 @@ std::chrono::milliseconds kTickPeriod{10};
 
 void loop()
 {
+
+  // throttle_position_signal = 420.0f;
   /**
    * You can use and set the CANSignals as if they were SignalType
    *
    */
-  engine_rpm_signal = 0.0f;
-  inlet_manifold_pressure_signal = 0.0f;
-  inlet_manifold_temperature_signal = 0.0f;
-  throttle_position_signal = 0.0f;
-  // float_tx_signal = 5.195f;
-  // uint8_t_tx_signal = 132;
-  // bool_tx_signal = true;
-  // millis_tx_signal = millis();
 
-  uint16_t local_engine_rpm_signal = engine_rpm_signal;
-  float local_inlet_manifold_pressure_signal = inlet_manifold_pressure_signal;
-  float local_inlet_manifold_temperature_signal = inlet_manifold_temperature_signal;
-  float local_throttle_position_signal = throttle_position_signal;
   // float test_float = float_rx_signal;
   // uint8_t test_uint8_t = uint8_t_rx_signal;
   // bool test_bool = bool_rx_signal;
   // uint32_t test_millis = millis_rx_signal;
 
+  
   can_bus.Tick();
+
+  if (millis() >= prev_tick + 100)
+  {
+    prev_tick = millis();
+    Serial.println(float(ecu_bat_voltage_signal));
+  }
+
+
 
   // The CANRXMessage automatically gets updated on message reception from the interrupt.
   // The CANTXMessage must be periodically ticked with an elapsed time in order to automatically be sent.
@@ -125,7 +134,7 @@ void loop()
     Serial.print(bool(test_bool));
     Serial.print(" Received millis: ");
     Serial.println(uint32_t(test_millis)); */
-    Serial.println(float(local_throttle_position_signal));
+    
 
   // tx_message.Tick(kTickPeriod);
   // next_tick_time =
